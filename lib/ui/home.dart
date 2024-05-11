5bc5c2327f0558b75186756dce17f3f4b9680bc1
Bruno Sanguinetti <brunoebarros@gmail.com>
2024-05-11 01:05:31 -0300

    feat: minimal mocked search

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+import 'search_bar.dart';
 import 'theme.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +14,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> _items = [
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Date",
+    "Fig",
+    "Grape",
+    "Lemon",
+    "Mango",
+    "Orange",
+    "Pineapple",
+    "Strawberry",
+    "Watermelon",
+  ];
+
+  void callback(String title) async {
+    debugPrint(title);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +40,13 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: CpTheme.of(context).backgroundColor,
         title: Text(widget.title),
       ),
-      body: const SizedBox(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SearchItemsInputField(),
+          ],
+        ),
+      ),
     );
   }
 }
